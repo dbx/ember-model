961f48bf27790d1d0791982ad87151c6c078764f
Toth David <tothda@gmail.com>
2014-06-08 18:52:35 +0200

    validációs hibák kezelése (status 400)

--- packages/ember-model/lib/rest_adapter.js
+++ packages/ember-model/lib/rest_adapter.js
@@ -148,9 +148,13 @@ Ember.RESTAdapter = Ember.Adapter.extend({
           jqXHR.then = null;
         }
 
-        Ember.run(null, reject, jqXHR);
+        // validációs hiba catch
+        if (jqXHR.status === 400) {
+          Ember.run(null, resolve, JSON.parse(jqXHR.responseText));
+        } else {
+          Ember.run(null, reject, jqXHR);
+        }
       };
-
 
       Ember.$.ajax(settings);
    });
